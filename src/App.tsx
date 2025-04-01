5011ef704a798ee1fdd7f1c647efa209e6187de8
Anna Ludwin <a.ludwin@getprintbox.com>
2025-04-01 12:59:14 +0200

    add product to list

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,74 @@ import { useState } from "react";
 import { ShoppingList } from "./components/ShoppingList";
 
 function App() {
+  // variables:
+
+  const [products, setProducts] = useState(["chleb", "sól", "wódka"]);
+  const [newProduct, setNewProduct] = useState("");
+
+  const ShoppingListItem = ({ product }) => {
+    return (
+      <div>
+        <input type="checkbox" />
+        <em>{product}</em>
+      </div>
+    );
+  };
+
+  // functions:
+
+  function changeInputText(e) {
+    setNewProduct(e.target.value);
+  }
+
+  function addProduct() {
+    setProducts([...products, newProduct]);
+    setNewProduct("");
+  }
+
+  function crossProduct() {}
+
+  function hideSelected() {}
+
+  // aplication:
+
   return (
     <>
-      <nav>
-        <h1>🔔BeL</h1>
-      </nav>
-      <ShoppingList />
+      <div className="screen">
+        <section className="day-panel dark">
+          <h3 className="shoppingList no-margin ">Shopping list</h3>
+
+          <input
+            type="text"
+            placeholder="enter a new product..."
+            className="custom-input custom-input:focus"
+            value={newProduct}
+            onChange={changeInputText}
+          />
+
+          <button className="shop" onClick={addProduct}>
+            +
+          </button>
+
+          <div
+            className="hide"
+            style={{ color: "blanchedalmond", marginTop: "1em" }}
+          >
+            <input type="checkbox" value="hide" />
+            <label>Hide selected</label>
+          </div>
+
+          <div className="meal-card list">
+            {products.map((product, index) => (
+              <ShoppingListItem
+                product={product}
+                key={index}
+              ></ShoppingListItem>
+            ))}
+          </div>
+          {console.log(newProduct)}
+        </section>
+      </div>
     </>
   );
 }
--- src/components/ShoppingList.tsx
+++ src/components/ShoppingList.tsx
@@ -3,25 +3,25 @@ export const ShoppingList = () => {
   return (
     <div className="screen">
       <section className="day-panel dark">
-        <h3 className="shoppingList no-margin ">Lista zakupów</h3>
+        <h3 className="shoppingList no-margin ">Shopping list</h3>
         <input
           type="text"
           placeholder="Dodaj produkt"
           className="custom-input custom-input:focus"
         />
-        <button className="shop">Dodaj produkt</button>
+        <button className="shop">Add new product</button>
 
         <div
           className="hide"
           style={{ color: "blanchedalmond", marginTop: "1em" }}
         >
           <input type="checkbox" value="hide" />
-          <label>ukryj zaznaczone</label>
+          <label>Hide selected</label>
         </div>
 
         <div className="meal-card list">
-          <ShoppingListItem product={"Produkt 1"}></ShoppingListItem>
-          <ShoppingListItem product={"Produkt 2"}></ShoppingListItem>
+          <ShoppingListItem product={"Product 1"}></ShoppingListItem>
+          <ShoppingListItem product={"Product 2"}></ShoppingListItem>
           <ShoppingListItem product={"..."}></ShoppingListItem>
         </div>
       </section>
